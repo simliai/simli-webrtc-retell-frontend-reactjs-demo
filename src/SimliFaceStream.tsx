ed8b7e49f8566aac979b16d8abda90dee66cd87c
AnasMations <anas.ahassan@yahoo.com>
2024-06-05 05:30:41 +0300

    Worklet

--- src/SimliFaceStream.tsx
+++ src/SimliFaceStream.tsx
@@ -6,6 +6,8 @@ import React, {
   useImperativeHandle,
 } from "react";
 
+import {moduleCode} from "./audioProcessor";
+
 interface ImageFrame {
   frameWidth: number;
   frameHeight: number;
@@ -45,6 +47,7 @@ const SimliFaceStream = forwardRef(
 
     // ------------------- AUDIO -------------------
     const audioContext = useRef<AudioContext | null>(null); // Ref for audio context
+    const audioNode = useRef<AudioWorkletNode | null>(null); // Ref for audio node
     const audioQueue = useRef<Array<AudioBuffer>>([]); // Ref for audio queue
 
     const accumulatedAudioBuffer = useRef<Uint8Array>(null); // Buffer for accumulating incoming data until it reaches the minimum size for decoding
@@ -67,35 +70,13 @@ const SimliFaceStream = forwardRef(
     const fps = 30;
     const frameInterval = 30; // Calculate the time between frames in milliseconds
 
-    /* Main loop */
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        if (audioQueueEmpty.current && !callCheckAndPlayFromQueueOnce.current) {
-          playAudioQueue();
-        }
-      }, playbackDelay + 10); // Add 10ms to the playback delay to give more time for chunk collection
-
-      return () => clearInterval(intervalId);
-    }, [
-      audioContext.current,
-      // NOTE: These values should be in the dependency array
-      // Just because the use effect depends on them.
-      playAudioQueue,
-      playbackDelay,
-    ]);
-
     /* Create AudioContext at the start */
     useEffect(() => {
       // Return if start is false
       if (start === false) return;
 
       // Initialize AudioContext
-      const newAudioContext = new AudioContext({
-        sampleRate: 16000,
-      });
-      audioContext.current = newAudioContext;
-
-      console.log("AudioContext created");
+      loadAudioWorklet();
 
       // Intialize VideoContext
       const videoCanvas = canvasRef.current;
@@ -109,6 +90,23 @@ const SimliFaceStream = forwardRef(
       if (ws.current && ws.current.readyState === WebSocket.OPEN) {
         ws.current.send(audioData);
         startTimeFirstByte.current = performance.now(); // Start time for first byte
+      }
+    };
+
+    const loadAudioWorklet = async () => {
+      try {
+        const newAudioContext = new AudioContext({sampleRate: 16000});
+        const blob = new Blob([moduleCode], { type: "application/javascript" });
+        const blobURL = URL.createObjectURL(blob);
+        await newAudioContext.audioWorklet.addModule(blobURL);
+        const newAudioNode = new AudioWorkletNode(newAudioContext, 'pcm-player');
+        newAudioNode.connect(newAudioContext.destination);
+    
+        audioContext.current = newAudioContext;
+        audioNode.current = newAudioNode;
+        console.log("AudioContext created");
+      } catch (error) {
+        console.error("Error loading AudioWorklet module:", error);
       }
     };
 
@@ -180,7 +178,6 @@ const SimliFaceStream = forwardRef(
 
       // Push image data to frame queue
       const imageFrame: ImageFrame = { frameWidth, frameHeight, imageData };
-      updateFrameQueue(imageFrame);
 
       // --------------- AUDIO DATA ----------------
 
@@ -192,8 +189,8 @@ const SimliFaceStream = forwardRef(
       // Extract Audio data
       const audioData = data.subarray(start_audioData, end_audioData);
 
-      // Push audio data to audio queue
-      updateAudioQueue(audioData);
+      // --------------- Update Audio and Video Queue ---------------
+      updateAudioAndVideoQueue(audioData, imageFrame);
 
       // --------------- LOGGING ----------------
 
@@ -301,176 +298,43 @@ const SimliFaceStream = forwardRef(
       await drawFrame();
     };
 
-    /* Update video queue */
-    const updateFrameQueue = async (imageFrame: ImageFrame) => {
+    const updateAudioAndVideoQueue = async (audioData: Uint8Array ,imageFrame: ImageFrame) => {
       if (numberOfChunksInQue.current >= minimumChunkSize) {
+        // Update Audio Queue
+        console.log("Sending audio to AudioWorkletNode:", accumulatedAudioBuffer.current);
+        audioNode.current.port.postMessage(accumulatedAudioBuffer.current);
+        accumulatedAudioBuffer.current = null;
+
+        // Update Frame Queue
         frameQueue.current.push(accumulatedFrameBuffer.current);
         accumulatedFrameBuffer.current = [];
+        playFrameQueue();
+
+        // Reset chunk size
+        numberOfChunksInQue.current = 0; 
       } else {
+        // Update Audio Buffer
+        if (!accumulatedAudioBuffer.current) {
+          accumulatedAudioBuffer.current = audioData;
+        } else {
+          const combinedUint8Array = new Uint8Array(
+            accumulatedAudioBuffer.current.length + audioData.length
+          );
+          combinedUint8Array.set(accumulatedAudioBuffer.current, 0);
+          combinedUint8Array.set(audioData, accumulatedAudioBuffer.current.length);
+          accumulatedAudioBuffer.current = combinedUint8Array;
+        }
+
+        // Update Frame Buffer
         accumulatedFrameBuffer.current.push(imageFrame);
       }
     };
-
-    /* Decode ArrayBuffer data to Audio and push to audio queue */
-    const updateAudioQueue = async (data: Uint8Array) => {
-      if (numberOfChunksInQue.current >= minimumChunkSize) {
-        console.log("1) Decoding audio at time: ", performance.now());
-        console.log(`|| QUEUE LENGTH: ${audioQueue.current.length} ||`);
-
-        // If the accumulated data reaches the minimum chunk size, decode and push to the queue
-        // Calculate the chunk collection time
-        executionTime.current = performance.now() - startTime.current;
-        console.log(
-          "Chunk collection time:",
-          executionTime.current / 1000,
-          "seconds"
-        );
-        startTime.current = null;
-        executionTime.current = 0;
-
-        // 1: Decode concatenated data as PCM16 audio
-        console.log("Decoding audio data", accumulatedAudioBuffer.current);
-        const decodedAudioData = await createAudioBufferFromPCM16(
-          accumulatedAudioBuffer.current
-        );
-
-        // 2: Reset accumulated data buffer
-        accumulatedAudioBuffer.current = null;
-
-        // 3: Push decoded audio data to the queue
-        audioQueue.current.push(decodedAudioData);
-
-        // 4: Check and Play only once at start
-        if (callCheckAndPlayFromQueueOnce.current) {
-          console.log("Checking and playing from queue ONCE");
-          callCheckAndPlayFromQueueOnce.current = false;
-          playAudioQueue();
-        }
-
-        numberOfChunksInQue.current = 0; // Reset chunk size
-      } else {
-        if (!accumulatedAudioBuffer.current) {
-          // If there is no accumulated data, set the data as the accumulated data
-          accumulatedAudioBuffer.current = data;
-        } else {
-          // Concatenate Uint8Arrays into a single Uint8Array
-          const combinedUint8Array = new Uint8Array(
-            accumulatedAudioBuffer.current.length + data.length
-          );
-          combinedUint8Array.set(accumulatedAudioBuffer.current, 0);
-          combinedUint8Array.set(data, accumulatedAudioBuffer.current.length);
-          accumulatedAudioBuffer.current = combinedUint8Array;
-        }
-      }
-    };
-
-    /* Helper function to decode ArrayBuffer as PCM16 */
-    // async function createAudioBufferFromPCM16(
-    //   input: Uint8Array
-    // ): Promise<AudioBuffer> {
-    //   // Ensure the input byte length is even
-    //   if (input.length % 2 !== 0) throw new Error("Input length must be even");
-
-    //   const numSamples = input.length / 2;
-    //   const audioBuffer = audioContext.current!.createBuffer(
-    //     1,
-    //     numSamples,
-    //     16000
-    //   );
-    //   const channelData = audioBuffer.getChannelData(0);
-
-    //   for (let i = 0, j = 0; i < input.length; i += 2, j++) {
-    //     // Little-endian byte order
-    //     let int16 = (input[i + 1] << 8) | input[i];
-    //     // Convert from uint16 to int16
-    //     if (int16 >= 0x8000) int16 |= ~0xffff;
-    //     // Normalize to range -1.0 to 1.0
-    //     channelData[j] = int16 / 32768.0;
-    //   }
-
-    //   return audioBuffer;
-    // }
-
-    /* Helper function to decode ArrayBuffer as PCM16 */
-    async function createAudioBufferFromPCM16(input: Uint8Array): Promise<AudioBuffer> {
-      // Ensure the input byte length is even
-      if (input.length % 2 !== 0) throw new Error("Input length must be even");
-    
-      const numSamples = input.length / 2;
-      const audioBuffer = audioContext.current!.createBuffer(1, numSamples, 16000);
-      const channelData = audioBuffer.getChannelData(0);
-    
-      // Convert Uint8Array to Int16Array
-      const int16Array = new Int16Array(numSamples);
-      for (let i = 0, j = 0; i < input.length; i += 2, j++) {
-        int16Array[j] = (input[i + 1] << 8) | input[i];
-      }
-    
-      // Normalize Int16Array to Float32Array
-      const float32Array = new Float32Array(numSamples);
-      for (let i = 0; i < numSamples; i++) {
-        float32Array[i] = int16Array[i] / 32768.0;
-      }
-    
-      // Copy Float32Array to channelData
-      channelData.set(float32Array);
-    
-      return audioBuffer;
-    }
 
     const sendSilence = () => {
       const silence = new Uint8Array(1068 * minimumChunkSize);
       ws.current?.send(silence);
       console.log("Sending silence!");
     };
-
-    /* Play audio in the queue */
-    async function playAudioQueue(): Promise<number> {
-      const audioBuffer = audioQueue.current.shift();
-      if (!audioBuffer) {
-        console.log("AudioBuffer is empty");
-        sendSilence();
-        audioQueueEmpty.current = true;
-        return 0;
-      } else {
-        playFrameQueue();
-      }
-      const source = audioContext.current!.createBufferSource();
-      source.buffer = audioBuffer;
-      // source.connect(audioContext.current!.destination);
-
-      // Create a gain node to control volume
-      const gainNode = audioContext.current!.createGain();
-      
-      // Amplify the audio volume
-      gainNode.gain.value = 2.0;
-      source.connect(gainNode);
-      gainNode.connect(audioContext.current!.destination);
-
-      // Calculate the time at which the last x ms of the audio starts
-      const lastmsStartTime = audioBuffer.duration - 0.020;
-
-      // Decrement the audio at the last x ms of audio
-      gainNode.gain.setTargetAtTime(0.5, audioContext.current!.currentTime + lastmsStartTime, 0.01);
-
-      // Start playback
-      console.log("2) Playing audio at time: ", performance.now());
-      source.start(0);
-
-      console.log(
-        `Playing audio: AudioDuration: ${audioBuffer!.duration.toFixed(2)}`
-      );
-
-      // Handle the audio end event
-      source.onended = () => {
-        console.log("Audio ended");
-        audioQueueEmpty.current = false;
-        playAudioQueue();
-      };
-
-      // Return back audio duration
-      return audioBuffer!.duration;
-    }
 
     return <canvas ref={canvasRef} width="512" height="512"></canvas>;
   }
